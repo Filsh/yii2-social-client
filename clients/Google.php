e93ebd16ae04cd28a66e3bdc49b2e69b600676c6
Maliy Igor <imaliy@maximagroup.com>
2014-07-23 09:46:49 +0300

    Fix google

--- clients/Google.php
+++ clients/Google.php
@@ -54,25 +54,8 @@ class Google extends \yii\authclient\clients\GoogleOAuth implements \filsh\yii2\
     
     public function getUserAvatar()
     {
-        $result = $this->getOauth2Service()->userinfo->get();
-        if(!empty($result['picture'])) {
-            return $result['picture'] . '?sz=500';
-        }
-        return null;
-    }
-    
-    /**
-     * @inheritdoc
-     */
-    protected function normalizeUserAttributes($attributes)
-    {
-        if(empty($attributes['birthday'])) {
-            $people = $this->getPlusService()->people->get($attributes['id']);
-            if(property_exists($people, 'birthday') && !empty($people->birthday)) {
-                list($attributes['birth_day'], $attributes['birth_month'], $attributes['birth_year']) = $this->parseBirthday($people->birthday);
-            }
-        }
-        return parent::normalizeUserAttributes($attributes);
+        $attr = $this->getUserAttributes();
+        return !empty($attr['image']) ? $attr['image'] : null;
     }
     
     private function getOauth2Service()
@@ -97,5 +80,73 @@ class Google extends \yii\authclient\clients\GoogleOAuth implements \filsh\yii2\
             $this->_calendarService = new \Google_Service_Calendar($this->_client);
         }
         return $this->_calendarService;
+    }
+    
+    /**
+     * @inheritdoc
+     */
+    protected function normalizeUserAttributes($attributes)
+    {
+        $this->normalizeEmail($attributes);
+        $this->normalizeName($attributes);
+        $this->normalizeBirthday($attributes);
+        $this->normalizeImage($attributes);
+        
+        return parent::normalizeUserAttributes($attributes);
+    }
+    
+    private function normalizeEmail(& $attributes)
+    {
+        if(!empty($attributes['emails'])) {
+            foreach($attributes['emails'] as $email) {
+                if($email['type'] === 'account') {
+                    $attributes['email'] = $email['value'];
+                    break;
+                }
+            }
+            if(empty($attributes['email'])) {
+                $attributes['email'] = $attributes['emails'][0]['value'];
+            }
+        }
+    }
+    
+    private function normalizeName(& $attributes)
+    {
+        if(!empty($attributes['name'])) {
+            $attributes['family_name'] = !empty($attributes['name']['familyName']) ? $attributes['name']['familyName'] : null;
+            $attributes['given_name'] = !empty($attributes['name']['givenName']) ? $attributes['name']['givenName'] : null;
+        }
+        if(empty($attributes['family_name']) && empty($attributes['given_name'])) {
+            list($attributes['family_name'], $attributes['given_name']) = explode(' ', $attributes['displayName']);
+        }
+    }
+    
+    private function normalizeBirthday(& $attributes)
+    {
+        $birthday = null;
+        if(empty($attributes['birthday'])) {
+            $people = $this->getPlusService()->people->get($attributes['id']);
+            if(property_exists($people, 'birthday') && !empty($people->birthday)) {
+                $birthday = $people->birthday;
+            }
+        } else {
+            $birthday = $attributes['birthday'];
+        }
+        
+        if($birthday !== null) {
+            list($attributes['birth_day'], $attributes['birth_month'], $attributes['birth_year']) = $this->parseBirthday($birthday);
+        }
+    }
+    
+    private function normalizeImage(& $attributes)
+    {
+        if(!empty($attributes['image']) && !$attributes['image']['isDefault']) {
+            $attributes['picture'] = explode('?sz', $attributes['image']['url'])[0];
+        } else {
+            $result = $this->getOauth2Service()->userinfo->get();
+            if(!empty($result['picture'])) {
+                $attributes['picture'] = $result['picture'];
+            }
+        }
     }
 }
